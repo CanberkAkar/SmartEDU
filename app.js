16997b082060beef622fde1c857c10c3fa397e7a
CanberkAkar <akarcanberk@gmail.com>
2024-06-12 17:26:34 +0300

    Partial, View and Public page created

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express= require('express');
 
+//NODEMON YÜKLERKEN NPM -İ -D NODEMON OLARAK YAZABİLİRİZ.
+//TypeError: fsevents.watch is not a function=> HATASI İÇİN npm audit fix --force kullandım
+const express= require('express');
+ 
 const app=express();
 
 const port=5050;
 
-//NODEMON YÜKLERKEN NPM -İ -D NODEMON OLARAK YAZABİLİRİZ.
-//TypeError: fsevents.watch is not a function=> HATASI İÇİN npm audit fix --force kullandım
+//TEMPLATE ENGINE
 
+app.set("view engine","ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+
+//ROUTES
 app.get('/',(req,res)=>{
-    res.status(200).send('INDEX');
+    res.status(200).render('index',{
+        page_name:"index"
+    });
 })
+app.get('/about',(req,res)=>{
+    res.status(200).render('about',{
+        page_name:"about"
+    });
+})
+
 app.listen(port,()=>{
     console.log(`App Started ${port}`);
 })
