7c90ae77087d893b3e8717c505506df4b433e9cc
CanberkAkar <akarcanberk@gmail.com>
2024-06-25 18:23:21 +0300

    User role created

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,14 @@
+
+//GELEN ROLLERE GÖRE İŞLEMLER YAPACAĞIZ.
+module.exports=(roles)=>{
+    return (req,res,next)=>
+    {
+        const userRole =req.body.role;
+        if(roles.includes(userRole)){
+            next();
+        }
+        else{
+            return res.status(401).send('YOU CANT DO IT');
+        }
+    }
+}
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,10 @@
 const express= require('express');
 const courseController= require("../controllers/courseController");
+const roleMiddleware= require('../middlewares/roleMiddleware');
 
 //APP JS'DEN BİR İSTEK GELİYOR.HTTP İSTEKLERİ İLGİLİ ROTALARA GÖNDERİYOR. ROUTER BUNU SAĞLIYOR.
 const router = express.Router();
-router.route('/').post(courseController.createCourse);//localhost:3000/courses eğer route'a yeni bir veri gelirse mesela '/add' gibi yolu artık /courses/add olur
+router.route('/').post(roleMiddleware(["teacher","admin"]),courseController.createCourse);//localhost:3000/courses eğer route'a yeni bir veri gelirse mesela '/add' gibi yolu artık /courses/add olur
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 
