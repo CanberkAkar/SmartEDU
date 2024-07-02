60b9b3e89aedd09e2bc5215ad1ab4227783d1836
CanberkAkar <akarcanberk@gmail.com>
2024-07-02 17:42:50 +0300

    Courses and user relationship is success

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 //COMMINTIY ISMI
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 
 const bcrypt= require('bcrypt');
 
@@ -57,11 +58,13 @@ exports.logoutUser = (req, res) => {
 }
   exports.getDashboardPage = async(req, res) => {
     const user= await User.findOne({_id:req.session.userID});
+    const courses= await Course.find({user:req.session.userID})
     const categories = await Category.find();
     res.status(200).render('dashboard', {
         page_name: "dashboard",
         user,
-        categories
+        categories,
+        courses
     });
 }
   
